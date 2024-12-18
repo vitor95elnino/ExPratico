8514e40ed7a32531573cfcc65297ce1ff2ee3032
torresj <jorge.torres@blip.pt>
2024-12-18 12:05:28 +0000

    Code reorganization and improvings

--- tla_decommission/src/tladecomm/get_sdn_rules.py
+++ tla_decommission/src/tladecomm/get_sdn_rules.py
@@ -1,33 +1,44 @@
-import os
-import requests
+from os import getenv
+from requests import post
 
-sourcegraph_api = os.getenv('SOURCEGRAPH_API')
-tla_name = os.getenv('TLA_NAME')  # tla to be decommissioned
-access_token = os.getenv('SOURCEGRAPH_TOKEN')
+class SourceGraphClient:
+    def __init__(self, sourcegraph_api: str, access_token: str):
+        self.sourcegraph_api = sourcegraph_api
+        self.access_token = access_token
 
-def get_sdn_rules(tla_name):
-    headers = {
-        'Authorization': 'token ' + access_token,
-        'Content-Type': 'application/json'
-    }
+    def get_sdn_rules(self, tla_name: str) -> dict:
+        headers = {
+            'Authorization': 'token ' + self.access_token,
+            'Content-Type': 'application/json'
+        }
 
-    data_sdn = f'{{"query":"query {{ search(query: \\"repo:^gitlab.app.betfair/i2/ file:sdn/defaults.yml target: tla_{tla_name} \\") {{ results {{ results {{ ... on FileMatch {{ repository {{ name }} file {{ path }} lineMatches {{ preview lineNumber }} }} }} }} }} }}","variables":null}}'
+        data_sdn = f'{{"query":"query {{ search(query: \\"repo:^gitlab.app.betfair/i2/ file:sdn/defaults.yml target: tla_{tla_name} \\") {{ results {{ results {{ ... on FileMatch {{ repository {{ name }} file {{ path }} lineMatches {{ preview lineNumber }} }} }} }} }} }}","variables":null}}'
 
-    response = requests.post(sourcegraph_api, data=data_sdn, headers=headers)
-    print(response.json())
-    return response.json()
+        response = post(sourcegraph_api, data=data_sdn, headers=headers)
+        print(response.json())  # @FIXME remove prints
+        return response.json()
 
-def process_data(data, tla_name):
-    tlas_using_target = []
-    for result in data['data']['search']['results']['results']:
-        print(result, '1')
-        for line_match in result['lineMatches']:
-            print(line_match, '2')
-            if f'target: tla_{tla_name}' in line_match['preview']:
-                tlas_using_target.append(result['repository']['name'].split('/')[-1])
-    return tlas_using_target
+    def process_data(self, data: dict, tla_name: str) -> list:
+        tlas_using_target = []
+        for result in data['data']['search']['results']['results']:
+            print(result, '1')
+            for line_match in result['lineMatches']:
+                print(line_match, '2')
+                if f'target: tla_{tla_name}' in line_match['preview']:
+                    tlas_using_target.append(result['repository']['name'].split('/')[-1])
+        return tlas_using_target
+
 
 if __name__ == '__main__':
-    data = get_sdn_rules(tla_name)
-    tlas = process_data(data, tla_name)
+    sourcegraph_api = getenv('SOURCEGRAPH_API')
+    access_token = getenv('SOURCEGRAPH_TOKEN')
+    tla_name = getenv('TLA_NAME', 'detestrg')
 
+    if not sourcegraph_api or not access_token:
+        print('Missing environment variables SOURCEGRAPH_API or SOURCEGRAPH_TOKEN')
+        exit(1)
+
+    sg_client = SourceGraphClient(sourcegraph_api, access_token)
+
+    data = sg_client.get_sdn_rules(tla_name)
+    tlas = sg_client.process_data(data, tla_name)
--- tla_decommission/src/tladecomm/sdn_email_helper.py
+++ tla_decommission/src/tladecomm/sdn_email_helper.py
@@ -2,36 +2,79 @@ import os
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from catoolkit.library.utils.loggable import Loggable
 from catoolkit.service.cmdb.insight_service import InsightService
 from catoolkit.service.cmdb.insight_service_arguments import InsightServiceArguments
-from tla_decommission.src.tladecomm.get_sdn_rules import tla_name
-from tla_decommission.src.tladecomm.sdn_helper import SdnHelper
-#JIRA CREDENTIALS
-jira_endpoint = os.getenv('JIRA_ENDPOINT')
-jira_username = os.getenv('JIRA_USERNAME')
-jira_password = os.getenv('JIRA_PASSWORD')
+from get_sdn_rules import SourceGraphClient
+from os import getenv
 
 def create_subject(tla):
     return f"Action Needed: Delete SDN Rule for the target {tla}"
 
 
-class EmailSdnHelper(SdnHelper):
-    def __init__(self, insight_service, smtp_config):
-        insight_service_args = InsightServiceArguments(
-            endpoint=jira_endpoint,
-            username=jira_username,
-            password=jira_password,
-            object_schema_id=11
-        )
-        insight_service = InsightService(insight_service_args)
-        super().__init__(insight_service)
+class EmailSdnHelper(Loggable):
+    def __init__(self, insight_service: InsightService, smtp_server: str, smtp_port: int, sender_email: str):
+        super().__init__()
+
+        self.job = None
+        self._insight_service = insight_service
+        self._sg_client = sg_client
 
         self.smtp_config = {
-            'smtp_server': os.getenv('SMTP_SERVER', 'ie1-mail-prd.prd.betfair'),
-            'port': int(os.getenv('SMTP_PORT', 25)),
-            'sender_email': os.getenv('SENDER_EMAIL', 'cloud.automation@paddypowerbetfair.com'),
+            'smtp_server': smtp_server,
+            'port': smtp_port,
+            'sender_email': sender_email,
         }
-        self.tla_name = os.getenv('TLA_NAME')
+
+    def fetch_sdn_rules(self, tla_name: str):
+        data = self._sg_client.get_sdn_rules(tla_name)
+        return self._sg_client.process_data(data, tla_name)
+
+    def get_tla_people(self, tla_name: str) -> dict:
+        all_people = {}
+        tla_list = self.fetch_sdn_rules(tla_name)
+        self._logger.info("Initial TLA list:", tla_list)
+
+        for tla in tla_list:
+            self._logger.info(f"Processing TLA: {tla}")
+            if tla in all_people:
+                self._logger.info(f"TLA {tla} already processed. Skipping.")
+                continue  #@FIXME ter a certeza que nao esta em loop
+
+            tla_people = self._insight_service.get_tla_people(tla)
+            all_people[tla] = tla_people
+            # Format the people data for stop the memory identifiers
+            formatted_people = {}
+            for key, value in tla_people.items():
+                if isinstance(value, list):  # Sees if is a list
+                    formatted_people[key] = [
+                        vars(person) if hasattr(person, "__dict__") else str(person) for person in value
+                    ]
+                else:
+                    formatted_people[key] = value  # For non-list values, keep as-is
+
+            all_people[tla] = formatted_people
+            self._logger.info(f"Processed TLA {tla} people: {formatted_people}")
+        return all_people
+
+    def get_emails(self, tla_name: str) -> list:
+        all_people = self.get_tla_people(tla_name)
+        emails = []
+        for tla, people_data in all_people.items():
+            for role, people_list in people_data.items():
+                if not isinstance(people_list, list):
+                    continue
+
+                # Extract emails from each person's attributes
+                for person in people_list:
+                    if not isinstance(person, dict):
+                        continue
+
+                    # @TODO test/check if the condition "email != 'N/A'" is needed
+                    email = person.get('email')
+                    if email and email != 'N/A':  # Only add valid emails
+                        emails.append(email)
+        return emails
 
     def send_email(self, tla_name):
         list_of_emails = self.get_sdn_emails()  # Get the emails for the current TLA
@@ -91,3 +134,32 @@ class EmailSdnHelper(SdnHelper):
 
         except Exception as e:
             print(f"An error occurred while sending email: {e}")
+
+if __name__ == '__main__':
+    smtp_server = os.getenv('SMTP_SERVER', 'ie1-mail-prd.prd.betfair')
+    port = int(os.getenv('SMTP_PORT', 25))
+    sender_email = os.getenv('SENDER_EMAIL', 'cloud.automation@paddypowerbetfair.com')
+
+    jira_endpoint = getenv('JIRA_ENDPOINT')
+    jira_username = getenv('JIRA_USERNAME')
+    jira_password = getenv('JIRA_PASSWORD')
+
+    sourcegraph_api = getenv('SOURCEGRAPH_API')
+    access_token = getenv('SOURCEGRAPH_TOKEN')
+
+    tla_name = getenv('TLA_NAME', 'detestrg')
+
+    # Create InsightServiceArguments with appropriate values
+    insight_service_args = InsightServiceArguments(
+        endpoint=jira_endpoint,
+        username=jira_username,
+        password=jira_password,
+        object_schema_id=11,
+    )
+
+    # Initialize the InsightService with valid arguments
+    insight_service = InsightService(insight_service_args)
+    sg_client = SourceGraphClient(sourcegraph_api, access_token)
+
+    esh = EmailSdnHelper(insight_service, smtp_server, port, sender_email)
+    esh.send_email(tla_name)
