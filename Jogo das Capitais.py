9a2cbb2cb15b82972cafd4c1534beac2f81246b1
vitor95elnino <122942604+vitor95elnino@users.noreply.github.com>
2025-01-05 12:42:30 +0000

    Add files via upload

--- Jogo das Capitais.py
+++ Jogo das Capitais.py
@@ -2,16 +2,20 @@ import random, os
 
 def carregar_txt():
     """
-    Abre o ficheiro jogo_das_capitais.txt e lê os valores de cada linha, divide em pais e capital, separados por uma virgula, 
-    e guarda-os num dicionário , retorna os paises como chave e as capitais como valores. 
-    Caso o ficheiro não exista, abre um novo ficheiro vazio. 
+    Esta função, carrega um dicionário de países e capitais a partir de um ficheiro .txt .
+    O codigo verifica se existe um ficheiro chamada "jogo_das_capitais.txt".
+    Se o ficheiro existir, lê o conteúdo e carrega as informações para um dicionário.
+    Se o ficheiro não existir, cria um novo ficheiro vazio, com o nome"jogo_das_capitais.txt e retorna um dicionário vazio."
+    O ficheiro deve ter cada linha no formato "país,capital", separado por uma vírgula.
+
+    Neste dicionário os paises são chaves e as capitais os valores.
     """
     jogo_das_capitais = {}
     if os.path.exists("jogo_das_capitais.txt"):
         with open("jogo_das_capitais.txt", "r", encoding="utf-8") as file:
             for line in file:
                 pais, capital = line.strip().split(",")
-                jogo_das_capitais[pais] = capital
+                jogo_das_capitais[pais.lower()] = capital.lower()
     else:
         print("Ficheiro não encontrado. A criar um novo ficheiro.")
         open("jogo_das_capitais.txt", "w").close()  
@@ -32,21 +36,22 @@ def guardar_dados(jogo_das_capitais):
 
 def adicionar(jogo_das_capitais):
     """
-    Adiciona novos paises e capitais ao jogo (jogo_das_capitais.txt).
-    Pede ao jogador para inserir o nome do país e verefica se já existe no dicionario.
-    Caso não exista, em seguida é pedido para inserir a capital. Caso seja inserido um pais já existente o processo volta ao inicio pedindo para insirir um novo país.
-    No dicionario .txt está a informação(país,capital) que é guardada numa linha separada por uma virgula.
-    Não aceita capitais nem paises "em branco" e numeros. Caso seja introduzido informa que o mesmo é invalido, e pede para ser insirido novamente. 
-    No final é pedido ao jogador se desaja continuar, caso este responda 'n' a função termina.
- 
+    Esta função permite ao jogador adicionar novos paises e capitais ao dicionário.
+    É Pedido ao jogador para inserir o nome do país e verefica se já existe no dicionario. 
+    No caso de ser inserido um pais já existente o processo volta ao inicio pedindo para insirir um novo país.
+    Caso não exista, em seguida é pedido para inserir a capital.
+    No dicionario está a informação(país,capital) que é guardada numa linha separada por uma virgula.
+    Não aceita capitais nem paises em branco e numeros. Caso seja introduzido informa que o mesmo é invalido, e pede para ser insirido novamente. 
+    A função 'ver_paises_capitais' permite ao jogador ver o dicionário.
+    Este ciclo termina quando o jogador não adicionar mais países e capitais.
     """
     while True:
         
         #País
         while True:
-            pais = input("Insira o nome do país: ").strip()
+            pais = input("Insira o nome de um país: ").strip().lower()
             if not pais or pais.isdigit():
-                print("Insira o nome do país,o campo não pode estar vazio, ou conter números.\n")
+                print("-> Insira o nome de um país,o campo não pode estar vazio, ou conter números.\n")
                 continue
             
             if pais in jogo_das_capitais:
@@ -56,81 +61,131 @@ def adicionar(jogo_das_capitais):
 
         #Capital    
         while True: 
-            capital = input("Insira o nome da capital de " + pais + ": ").strip()
+            capital = input("Insira a capital de " + pais + ": ").strip().lower()
             if not capital or capital.isdigit():
-                print("Insira uma capital Valida, este campo não pode ser vazio, ou conter números.\n")
+                print("-> Insira uma capital valida, este campo não pode ser vazio, ou conter números.\n")
             else:
                 break
 
         jogo_das_capitais[pais] = capital
         guardar_dados(jogo_das_capitais)
-        print(pais + " e a sua capital " + capital + " foram adicionados!\n")
+        print("A capital do "+ pais + ", " + capital + " foram adicionadas!\n")
+        # Ver Dicionário
+        while True:
+            opcao = input("\n-> Deseja ver o dicionário ? (s/n): ").strip()
+            if opcao == 's':
+                ver_paises_capitais(jogo_das_capitais)
+                break
+            elif opcao == 'n':
+                break
+            else:
+                print("-> Opção inválida. Deseja ver o dicionário ? (s/n): ")
 
         #Sair
-        continuar = input("\nDeseja adicionar mais países e capitais? (s/n): ").strip()
+        continuar = input("\n-> Deseja adicionar mais países e capitais? (s/n): ").strip()
         if continuar in ['s', 'n']:
             if continuar == 'n':
+                break
+        else:
+            print("-> Opção Invalida. Deseja adicionar mais países e capitais? (s/n): ")  
+
+
+def jogar_jogo(jogo_das_capitais, nome=None):
+    """
+    Função para iniciar o jogo. 
+    Verifica se o dicionário tem pelo menos 10 paises e capitais.
+    Se não existir, o jogador é avisado que para começar a jogar terá que adicionar mais paises e capitais ao dicionário e é chamada a função adicionar.
+    Caso existam pelo menos 10, é usado um random para seleciona aleatoriamente 10 paises e perguntar as suas capitais. 
+    Por cada resposta certa o jogador recebe 1 ponto. Esse ponto é somado a variavel pontuacao que inicia a 0.
+    O jogador é informado sempre que acerta ou erra uma capital.
+    Por ultimo é mostrado no ecrã quantas capitais o jogador respondeu correto(pontuação) e o total de perguntas (total_perguntas = len(jogo_das_capitais)), 
+    é perguntado ao jogador se deseja continuar, caso o jogador continue a jogar, o nome do jogador fornecido no parâmetro 'nome' é reutilizado.
+    """
+
+    if len(jogo_das_capitais) < 10:
+        print("Não há dados disponiveis. Precisa de ter 10 países e as respetivas capitais para começar a jogar. \n")
+        adicionar(jogo_das_capitais)
+        return
+
+    if nome is None:
+        while True:
+            nome = input("Insira o nome do Jogador: ").strip().lower()
+            if nome:
+                break
+            else:
+                print("Insira o nome do Jogador, este campo não pode estar em branco.\n")
+
+    pontuacao = 0
+    total_capitais = min(10, len(jogo_das_capitais))
+    perguntas = random.sample(list(jogo_das_capitais.items()), total_capitais)
+
+    for pais, capital in perguntas:
+        resposta = input("\nQual é a capital de "  + pais + "? ").strip().lower()
+        if resposta.lower() == capital:
+            print("Correto!")
+            pontuacao += 1
+        else:
+           print("Errado.\nA capital é " + capital + ".")
+    print("\n"+ str(nome) + " acertou em " + str(pontuacao) + " de " + str(total_capitais) + " capitais.\n")
+
+    while True:
+        continuar = input("\n->"+nome+ " deseja continuar a jogar ? (s/n): ").strip().lower()
+        if continuar in ['s', 'n']:
+            if continuar == 's':
+                jogar_jogo(jogo_das_capitais, nome)
+                break
+            elif continuar == 'n':
                 break
         else:
             print("Opção Invalida. Deseja adicionar mais países e capitais? (s/n): ")  
 
 
-def jogar_quiz(jogo_das_capitais):
+def ver_paises_capitais(jogo_das_capitais):
     """
-    Função para iniciar o jogo. 
-    Verifica no dicionário se há pelo menos 10 paises e capitais no dicionário.
-    Se não existir, o jogador é avisado que para começar a jogar terá que adcionar mais paises e capitais ao dicionário.
-    Caso exista 10 ou mais, a função atráves do "random" seleciona aleatoriamente 10 paises e pergunta as suas capitais. 
-    A função atrás da variavel pontuação, que inicia a 0, sempre que o jogador acerta uma capital, é adicionado +1 ponto a variavel pontuação, caso o jogador responda errado, é 
-    mostrado no ecrã resposta errada e a respectiva resposta correta.
-    No final é mostrado no ecrã quantas capitais o jogador respondeu correto(pontuação) e o total de perguntas (total_perguntas = len(jogo_das_capitais))
+    Função para visualizar conteúdo do dicionário.
+    Exibe no ecrã o conteúdo do dicionario 'jogo_das_capitais', após ser exibido no ecrã aguarda uma instrução do utilizador para avançar.
     """
+    with open("jogo_das_capitais.txt", "r", encoding="utf-8") as file:
+        conteudo = file.read()
+        print("\nConteúdo do arquivo jogo_das_capitais.txt:\n")
+        print(conteudo)
 
-    if len(jogo_das_capitais) < 10:
-        print("Não há dados disponiveis. Adicione pelo menos 10 países e capitais para começar a jogar. \n")
-        return
-    nome = input("Insira o nome do Jogador: ")
-
-    pontuacao = 0
-    total_perguntas = min(10, len(jogo_das_capitais))
-    perguntas = random.sample(list(jogo_das_capitais.items()), total_perguntas)
-
-    for pais, capital in perguntas:
-        resposta = input("Qual é a capital de "  + pais + "? ").strip()
-        if resposta.lower() == capital.lower():
-            print("Correto!\n")
-            pontuacao += 1
-        else:
-           print("Errado. A capital é " + capital + ".\n")
-    print(str(nome) + " acertou " + str(pontuacao) + " em " + str(total_perguntas) + " capitais.\n")
-
+    while True:
+        voltar = input("\nPressione ENTER para voltar atrás ")
+        return 
+        
 
 def main():
     """
     Função principal
     Começa por carregar os dados do dicionario.
-    Exibe um menu de seleção(1,2,3)que permite:
-        1 - Jogar: permite ao jogador começar o Quiz. 
-        2- Adicionar: utiliza a função ... para permitir ao jogador adicionar novos países e capitais.
-        3- Sair: sai do programa
+    Exibe um menu de seleção(1,2,3,4)que permite:
+        1- Jogar: Permite ao jogador começar a jogar. 
+        2- Adicionar: Permite ao jogador adicionar novos países e capitais ao dicionário. 
+        3- Ver dicionário: Chama a função ver_paises_capitais para o jogador ver o dicionário.
+        4- Sair: Sai do programa.
         Caso de selecionar outra opção, aparece no ecrã a msg:"Opção inválida. Tente novamente."
     """
     jogo_das_capitais = carregar_txt()
 
     while True:
         
-        print ("\nMenu: Quiz das Capitais \n")
-        print ("-> 1 - Jogar")
-        print ("-> 2 - Adicionar País e Capital")
-        print ("-> 3 - Sair \n")
+        print("\nBem-Vindo ao Jogo das Capitais! \n")
+        print("Selecione uma das opções para começar.\n")
+        print("-> 1 - Jogar")
+        print("-> 2 - Adicionar País e Capital")
+        print("-> 3 - Ver dicionário")
+        print("-> 4 - Sair \n")
         
         opcao = int(input("Escolha uma opção: "))
         
         if opcao == 1:
-            jogar_quiz(jogo_das_capitais)
+            jogar_jogo(jogo_das_capitais)
         elif opcao == 2:
             adicionar(jogo_das_capitais)
         elif opcao == 3:
+            ver_paises_capitais(jogo_das_capitais)
+        elif opcao == 4:
             print("Adeus, Volte mais Tarde")
             break
         else:
